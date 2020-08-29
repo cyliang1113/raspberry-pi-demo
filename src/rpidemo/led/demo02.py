6be2127d3fdce2a8940d226e5bd9f48229f59893
leon <cyliang1113@126.com>
2020-08-29 21:38:54 +0800

    11111

--- src/rpidemo/led/demo02.py
+++ src/rpidemo/led/demo02.py
@@ -2,14 +2,16 @@ import RPi.GPIO as GPIO  # 导入模块
 import time
 
 
+
 GPIO.setmode(GPIO.BCM)  # 设置gpio引脚编号编码(BCM, BOARD) gpio.jpg
-GPIO.setup(18, GPIO.OUT)  # 18号引脚设置为输出
+pin = 18  # 18号引脚
+GPIO.setup(pin, GPIO.OUT)  # 引脚设置为输出
 
 try:
     for i in range(10):
-        GPIO.output(18, True)  # 18号引脚高电平
+        GPIO.output(pin, True)  # 18号引脚高电平
         time.sleep(0.5)
-        GPIO.output(18, False)
+        GPIO.output(pin, False)
         time.sleep(0.5)
 finally:
     GPIO.cleanup()  # 程序异常 清除引脚上的电平 防止下次使用时, 出现意外
---
+++ src/rpidemo/led/demo03_pwm.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO  # 导入模块
+import time
+
+
+
+GPIO.setmode(GPIO.BCM)  # 设置gpio引脚编号编码(BCM, BOARD) gpio.jpg
+pin = 18  # 18号引脚
+GPIO.setup(pin, GPIO.OUT)  # 18号引脚设置为输出
+pwm = GPIO.PWM(pin, 500)  # 创建pwm 频率为500Hz 树莓派的pwm超过500Hz稳定性差
+
+try:
+    pwm.start(100)  # 开始脉冲, 占空比为100
+    while True:
+        duty = input("输入(0 - 100): ")
+        pwm.ChangeDutyCycle(int(duty))
+finally:
+    GPIO.cleanup()  # 程序异常 清除引脚上的电平 防止下次使用时, 出现意外
+    print("GPIO.cleanup()")
