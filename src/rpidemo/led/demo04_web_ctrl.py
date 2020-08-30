277d710e3526149a964fc3953e5b80a3f76b021b
leon <cyliang1113@126.com>
2020-08-30 16:54:19 +0800

    11111

--- src/rpidemo/led/demo04_web_ctrl.py
+++ src/rpidemo/led/demo04_web_ctrl.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)  # 设置gpio引脚编号编码(BCM, BOARD) gpio.jpg
 pin = 18  # 18号引脚
+GPIO.setwarnings(False)
 GPIO.setup(pin, GPIO.OUT)  # 18号引脚设置为输出
 pwm = GPIO.PWM(pin, 500)  # 创建pwm 频率为500Hz 树莓派的pwm超过500Hz稳定性差
 dutyValue = 50
