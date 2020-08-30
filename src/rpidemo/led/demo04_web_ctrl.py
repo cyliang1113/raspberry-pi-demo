4dbbd5e5b47312594e80025212d3a1dc80161abb
leon <cyliang1113@126.com>
2020-08-30 16:48:17 +0800

    11111

--- src/rpidemo/led/demo04_web_ctrl.py
+++ src/rpidemo/led/demo04_web_ctrl.py
@@ -6,7 +6,7 @@ pin = 18  # 18号引脚
 GPIO.setup(pin, GPIO.OUT)  # 18号引脚设置为输出
 pwm = GPIO.PWM(pin, 500)  # 创建pwm 频率为500Hz 树莓派的pwm超过500Hz稳定性差
 dutyValue = 50
-d = 10
+d = 20
 pwm.start(dutyValue)  # 开始脉冲, 占空比为0
 
 def add():
