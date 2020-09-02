413e9539aa938217e0e990bd2cb082a52f5eca84
leon <cyliang1113@126.com>
2020-09-02 22:07:24 +0800

    11111

--- src/rpidemo/motor/demo01.py
+++ src/rpidemo/motor/demo01.py
@@ -6,9 +6,13 @@ import time
 GPIO.setmode(GPIO.BCM)  # 设置gpio引脚编号编码(BCM, BOARD) gpio.jpg
 pin26_in1 = 26;
 pin19_in2 = 19;
+pin13_ena = 19;
 GPIO.setup(pin26_in1, GPIO.OUT)
 GPIO.setup(pin19_in2, GPIO.OUT)
+GPIO.setup(pin13_ena, GPIO.OUT)
 try:
+    GPIO.output(pin13_ena, True)
+
     GPIO.output(pin26_in1, True)
     GPIO.output(pin19_in2, False)
     time.sleep(2)
