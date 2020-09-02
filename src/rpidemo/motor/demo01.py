96c7c90b83cd8b85c5ce6980eca68fb15c58fcba
leon <cyliang1113@126.com>
2020-09-02 22:01:57 +0800

    11111

---
+++ src/rpidemo/motor/demo01.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO  # 导入模块
+import time
+
+# L298N
+
+GPIO.setmode(GPIO.BCM)  # 设置gpio引脚编号编码(BCM, BOARD) gpio.jpg
+pin26_in1 = 26;
+pin19_in2 = 19;
+GPIO.setup(pin26_in1, GPIO.OUT)
+GPIO.setup(pin19_in2, GPIO.OUT)
+try:
+    GPIO.output(pin26_in1, True)
+    GPIO.output(pin19_in2, False)
+    time.sleep(2)
+    GPIO.output(pin26_in1, False)
+    GPIO.output(pin19_in2, True)
+    time.sleep(2)
+finally:
+    GPIO.cleanup()  # 程序异常 清除引脚上的电平 防止下次使用时, 出现意外
+    print("GPIO.cleanup()")
