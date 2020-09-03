471e52d56c38204d5874352625d23ffd0f729967
leon <cyliang1113@126.com>
2020-09-03 16:47:56 +0800

    11111

--- src/rpidemo/bottle/demo01.py
+++ src/rpidemo/bottle/demo01.py
@@ -1,11 +1,14 @@
 from bottle import route, run, template, static_file
 
+
 @route('/')
 def hello():
     return "Hello World!"
 
-@route('/html/<path>')
-def page(path):
-    return static_file(path, root='./html/')
+
+@route('/html/<filepath:path>')
+def page(filepath):
+    return static_file(filepath, root='./html/')
+
 
 run(host='0.0.0.0', port=8080)
