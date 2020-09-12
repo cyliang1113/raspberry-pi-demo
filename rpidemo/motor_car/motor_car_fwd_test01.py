978085cb9e069837ae0467c7ec3153eba9aa7f77
leon <cyliang1113@126.com>
2020-09-12 17:44:39 +0800

    11111

--- rpidemo/motor_car/motor_car_fwd_test01.py
+++ rpidemo/motor_car/motor_car_fwd_test01.py
@@ -15,7 +15,7 @@ pin_right_motor_in1 = 20
 pin_right_motor_in2 = 21
 
 fwd = FWD(pin_left_motor_en, pin_left_motor_in1, pin_left_motor_in2,
-          pin_right_motor_en, pin_left_motor_in1, pin_right_motor_in2)
+          pin_right_motor_en, pin_right_motor_in1, pin_right_motor_in2)
 fwd.launch()
 fwd.forward()
 time.sleep(2)
